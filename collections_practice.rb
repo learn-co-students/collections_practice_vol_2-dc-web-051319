51f8b32ad90f0ce04c85bbe1333e3aed71860c83
Matt Williams <matt.seongjun@gmail.com>
2019-04-03 11:13:05 -0400

    Finally #count_elements done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+    array.select{ |ele| ele.to_str.downcase[0] != 'r' } == []
+end
+
+def contain_a(array)
+    array.select{ |ele| ele.to_str.downcase.include? 'a' }
+end
+
+def first_wa(array)
+    array.find{ |ele| ele.to_s.downcase.include? 'wa' }
+end
+
+def remove_non_strings(array)
+    array.select{ |ele| ele.is_a? String }
+end
+
+def count_elements(array)
+    array.each_with_object({}) do |ele, hash|
+        if hash[ele[:name]]
+            hash[ele[:name]] += 1
+        else 
+            hash[ele[:name]] = 1
+        end
+    end.map{ |name, count| {name: name, count: count}}
+end
+
+def merge_data(keys, data)
+        
+end
+
+def find_cool(array)
+    rtn = []
+    array.each do |hash|
+        if hash[:temperature] == "cool" 
+            rtn << hash
+        end
+    end
+    rtn
+end
+
+def organize_schools
+end
